f7b7c4dfcf574c695c42070bdc04c53601d0492e
Niu Jinshuang <niujinshuang@Nius-MacBook-Pro.local>
2023-03-10 12:18:43 -0800

    peer sucess

--- README.md
+++ README.md
@@ -19,8 +19,125 @@ terraform apply
 
 terraform destroy
 
-2. create the k8s cluster in  cluster_vpc
+save the cluter-vpc, subnets, utility subnets info.
+
+2. create the k8s cluster in  cluster_vpc 
+
+# k8s_aws
+
+ssh-keygen -t rsa -f ~/.ssh/k8s_key -P ""
+
+export AWS_REGION=us-east-1 
+export AWS_PROFILE=k8s
+
+export NAME=k8s.csye6225jinshuang.me
+export KOPS_STATE_STORE=s3://jin-k8s-com-state-store
+<!-- get from network creation -->
+export VPC_ID=vpc-00c9b74d181612a19
+
+db_endpoint = "todo.cob9le38a3hn.us-east-1.rds.amazonaws.com:3306"
+mysql -h todo.cob9le38a3hn.us-east-1.rds.amazonaws.com -P 3306 -u root -p
+
+kops create cluster \
+--cloud=aws \
+--master-zones=us-east-1a,us-east-1b,us-east-1c \
+--zones=us-east-1a,us-east-1b,us-east-1c \
+--node-count=2 \
+--topology private \
+--subnets=subnet-0e2dcb25834bb792d,subnet-0bb6c837809f8f394,subnet-088e60970dcc67124 \
+--utility-subnets=subnet-002de0069b2fd4f06,subnet-002e707227708ac49,subnet-08e5780ebe1569988 \
+--ssh-public-key ~/.ssh/k8s_key.pub \
+--authorization RBAC \
+--networking canal \
+--node-size=t3.medium \
+--master-size=t3.medium \
+--out=. \
+--target=terraform \
+--vpc=${VPC_ID} \
+${NAME}
 
 
+terraform init
+terraform apply
+
+% kops create secret sshpublickey admin -i ~/.ssh/k8s_key.pub --name ${NAME}
+
+% kops update cluster ${NAME} --yes --out=. --target=terraform 
+
+terraform init 
+terraform apply
+
+<!-- kops export kubecfg --admin
+
+kops validate cluster
+
+kops update cluster ${NAME} --yes --out=. --target=terraform  -->
+
+<!-- 
+
+kops validate cluster
+
+grep server ~/.kube/config -->
+
+-------- / add bastion host / -----------
+
+#### create the kubernetes secrete to access the cluster from bastion host:
+
+kubectl create secret generic ssh-key-secret --from-file=id_rsa=/Users/niujinshuang/.ssh/k8s_key
+
+kops update cluster ${NAME} --yes --out=. --target=terraform 
+
+terraform apply
+
+#### create the bastion host:
+
+kops create instancegroup bastions --role Bastion --subnet utility-us-east-1c --name ${NAME}
+
+kops update cluster ${NAME} --yes --out=. --target=terraform 
+
+<!-- kops validate cluster -->
+
+terraform init
+terraform apply
+
+bastion_elb_url=`aws elb --output=table describe-load-balancers|grep DNSName.\*bastion|awk '{print $4}'`
+
+ssh -i ~/.ssh/k8s_key ubuntu@${bastion_elb_url}
+
+
+<!-- ssh admin@i-0175b1819c10720a4 -->
+
+-------- / delete resources and cluster / ------------ 
+
+terraform destroy
+kops delete cluster --name ${NAME} --yes 
+
+<!-- Verify you have an SSH agent running. This should match whatever you built your cluster with.
+ssh-add -l
+If you need to add the key to your agent:
+ssh-add path/to/private/key
+
+Now you can SSH into the bastion
+ssh -A admin@<bastion-ELB-address>
+
+Where <bastion-ELB-address> is usually bastion.$clustername (bastion.example.kubernetes.cluster) unless otherwise specified -->
+
+#### test vpc peering connection by connecting to RDS database from bastion host: 
+
+sudo apt update
+sudo apt install mysql-server
+mysql -h todo.cob9le38a3hn.us-east-1.rds.amazonaws.com -P 3306 -u root -p
+
+
+
+kubectl create secret generic ssh-key-secret --from-file=id_rsa=~/.ssh/k8s_key
+
+kubectl create secret generic ssh-key-secret --from-file=id_rsa=/Users/niujinshuang/.ssh/k8s_key
+
+
+---------- / add init container / -----------
+
+kubectl apply -f init2.yml
+kubectl logs myapp-pod -c schema-migration
 
 
--- network_permissions.tf
+++ network_permissions.tf
@@ -1,36 +1,202 @@
 provider "aws" {
   region = "us-east-1"
 }
+#   -----------------  Create a cluster VPC ---------------------
+resource "aws_vpc" "cluster_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "cluster_vpc"
+  }
 
-provider "aws" {
-  alias  = "accepter"
-  region = "us-east-1"
 }
 
-# ---------- database network resources in database_vpc ----------------
+# Create an Internet Gateway and attach it to the VPC
+resource "aws_internet_gateway" "cluster_igw" {
+  vpc_id = aws_vpc.cluster_vpc.id
+}
 
-# Create VPC
+# Create subnets in the VPC
+resource "aws_subnet" "cluster_subnet_1" {
+  vpc_id = aws_vpc.cluster_vpc.id
+  cidr_block = "10.0.32.0/19"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "cluster_subnet_2" {
+  vpc_id = aws_vpc.cluster_vpc.id
+  cidr_block = "10.0.64.0/19"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "cluster_subnet_3" {
+  vpc_id = aws_vpc.cluster_vpc.id
+  cidr_block = "10.0.96.0/19"
+  availability_zone = "us-east-1c"
+}
+
+# Create utility subnets in the VPC
+resource "aws_subnet" "utility_subnet_1" {
+  vpc_id = aws_vpc.cluster_vpc.id
+  cidr_block = "10.0.4.0/22"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "utility_subnet_2" {
+  vpc_id = aws_vpc.cluster_vpc.id
+  cidr_block = "10.0.8.0/22"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "utility_subnet_3" {
+  vpc_id = aws_vpc.cluster_vpc.id
+  cidr_block =  "10.0.24.0/22"
+  availability_zone = "us-east-1c"
+}
+
+# Create a route table for the VPC
+resource "aws_route_table" "cluster_rt" {
+  vpc_id = aws_vpc.cluster_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cluster_igw.id
+  }
+}
+
+# Associate the subnets with the route table
+resource "aws_route_table_association" "utility_subnet_1_assoc" {
+  subnet_id = aws_subnet.utility_subnet_1.id
+  route_table_id = aws_route_table.cluster_rt.id
+}
+
+resource "aws_route_table_association" "utility_subnet_2_assoc" {
+  subnet_id = aws_subnet.utility_subnet_2.id
+  route_table_id = aws_route_table.cluster_rt.id
+}
+
+# Associate the subnets with the route table
+resource "aws_route_table_association" "utility_subnet_3_assoc" {
+  subnet_id = aws_subnet.utility_subnet_3.id
+  route_table_id = aws_route_table.cluster_rt.id
+}
+
+
+
+# ------------------------ create a database vpc ---------------------
+
+# Create a VPC
 resource "aws_vpc" "database_vpc" {
-  cidr_block = "10.0.0.0/16"
-    tags = {
+  cidr_block = "192.168.0.0/16"
+  tags = {
     Name = "database_vpc"
   }
 }
 
-# Create subnets
-resource "aws_subnet" "subnet1" {
-  cidr_block = "10.0.1.0/24"
+# Create subnets in the VPC
+resource "aws_subnet" "database_subnet_1" {
   vpc_id = aws_vpc.database_vpc.id
-  availability_zone = "us-east-1a"
+  cidr_block = "192.168.1.0/24"
 }
 
-resource "aws_subnet" "subnet2" {
-  cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "database_subnet_2" {
   vpc_id = aws_vpc.database_vpc.id
-  availability_zone = "us-east-1b"
+  cidr_block = "192.168.2.0/24"
 }
 
-# Create security group for RDS
+resource "aws_subnet" "database_subnet_3" {
+  vpc_id = aws_vpc.database_vpc.id
+  cidr_block = "192.168.3.0/24"
+  availability_zone = "us-east-1c"
+}
+
+resource "aws_subnet" "database_subnet_4" {
+  vpc_id = aws_vpc.database_vpc.id
+  cidr_block = "192.168.4.0/24"
+  availability_zone = "us-east-1d"
+}
+
+# Create an Internet Gateway and attach it to the VPC
+resource "aws_internet_gateway" "database_igw" {
+  vpc_id = aws_vpc.database_vpc.id
+}
+
+# Create a route table for the VPC
+resource "aws_route_table" "database_rt" {
+  vpc_id = aws_vpc.database_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.database_igw.id
+  }
+}
+
+# Associate the subnets with the route table
+resource "aws_route_table_association" "database_subnet_1_assoc" {
+  subnet_id = aws_subnet.database_subnet_1.id
+  route_table_id = aws_route_table.database_rt.id
+}
+
+resource "aws_route_table_association" "database_subnet_2_assoc" {
+  subnet_id = aws_subnet.database_subnet_2.id
+  route_table_id = aws_route_table.database_rt.id
+}
+
+# Associate the subnets with the route table
+resource "aws_route_table_association" "database_subnet_3_assoc" {
+  subnet_id = aws_subnet.database_subnet_3.id
+  route_table_id = aws_route_table.database_rt.id
+}
+
+resource "aws_route_table_association" "database_subnet_4_assoc" {
+  subnet_id = aws_subnet.database_subnet_4.id
+  route_table_id = aws_route_table.database_rt.id
+}
+
+# ----------------------- vpc peering ---------------------
+
+resource "aws_vpc_peering_connection" "peer" {
+  vpc_id      = aws_vpc.cluster_vpc.id
+  peer_vpc_id = aws_vpc.database_vpc.id
+  tags = {
+    Name = "vpc-peering"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "accepter" {
+
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  auto_accept = true
+
+  tags = {
+    Name = "accepted-peer-connection"
+  }
+}
+
+resource "aws_route" "cluster2db" {
+  route_table_id = aws_route_table.cluster_rt.id
+  destination_cidr_block = aws_vpc.database_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+# resource "aws_route" "cluster2db1" {
+#   route_table_id = "rtb-0cb461214a00426a9"
+#   destination_cidr_block = aws_vpc.database_vpc.cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+# }
+
+resource "aws_route" "db2cluster" {
+  route_table_id = aws_route_table.database_rt.id
+  destination_cidr_block = aws_vpc.cluster_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "db2cluster1" {
+  route_table_id = aws_vpc.database_vpc.main_route_table_id
+  destination_cidr_block = aws_vpc.cluster_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+
+# --------------- create database ------------------
+
 resource "aws_security_group" "rds" {
   name_prefix = "rds-"
 
@@ -40,66 +206,29 @@ resource "aws_security_group" "rds" {
     protocol = "tcp"
     cidr_blocks = [aws_vpc.cluster_vpc.cidr_block]
   }
-  
+  # cidr_blocks = [aws_vpc.cluster_vpc.cidr_block]
+  # cidr_blocks = ["0.0.0.0/0"]
   vpc_id = aws_vpc.database_vpc.id
 }
-
-
 
 # Create DB subnet group
 resource "aws_db_subnet_group" "database_vpc" {
   name = "todo-subnet-group"
-  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  subnet_ids = [
+    aws_subnet.database_subnet_1.id,
+    aws_subnet.database_subnet_2.id,
+    aws_subnet.database_subnet_3.id,
+    aws_subnet.database_subnet_4.id
+  ]
 }
 
-
-# ---------- cluster network resources in cluster_vpc ---------------
-
-resource "aws_vpc" "cluster_vpc" {
-  cidr_block = "192.168.0.0/16"
-  tags = {
-    Name = "cluster-vpc"
-  }
-}
-
-resource "aws_internet_gateway" "kops_cluster_igw" {
-  vpc_id = aws_vpc.cluster_vpc.id
-  tags = {
-    Name = "cluster_igw"
-  }
-}
-
-# --------------------- vpc peering-----------------
-
-resource "aws_vpc_peering_connection" "peer" {
-  peer_vpc_id = aws_vpc.database_vpc.id
-  vpc_id      = aws_vpc.cluster_vpc.id
-
-  auto_accept = true
-
-  tags = {
-    Name = "vpc-peering"
-  }
-}
-
-resource "aws_vpc_peering_connection_accepter" "peer_accept" {
-  provider      = aws.accepter
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-
-  tags = {
-    Name = "vpc-peering-accept"
-  }
-}
-
-
-# ----------------------- Create RDS instance ---------------
 resource "aws_db_instance" "RDSmySql" {
   identifier = "todo"
   allocated_storage = 20
   engine = "mysql"
-  engine_version = "5.7"
+  engine_version = "8.0.31"
   instance_class = "db.t2.micro"
-  name = "todo"
+  db_name = "todo"
   username = "root"
   password = "password"
   skip_final_snapshot = true
@@ -108,11 +237,38 @@ resource "aws_db_instance" "RDSmySql" {
 }
 
 
-# -------------- outputs to create kubernete cluster ---------------------
+
+# ----------------------------- outputs---------------------------------
 
 output "cluster_vpc_id" {
   value = aws_vpc.cluster_vpc.id
 }
 
+output "db_endpoint" {
+  value = aws_db_instance.RDSmySql.endpoint
+}
 
+output "subnet1" {
+  value = aws_subnet.cluster_subnet_1.id
+}
+
+output "subnet2" {
+  value = aws_subnet.cluster_subnet_2.id
+}
+
+output "subnet3" {
+  value = aws_subnet.cluster_subnet_3.id
+}
+
+output "utility1" {
+  value = aws_subnet.utility_subnet_1.id
+}
+
+output "utility2" {
+  value = aws_subnet.utility_subnet_2.id
+}
+
+output "utility3" {
+  value = aws_subnet.utility_subnet_3.id
+}
 
